1d8af0006e8ca4cdc835bf77dce80094dce964f0
maryblessing-tech <isolokwu.obiagelimaryblessing@gmail.com>
2026-01-19 19:33:09 +0100

    Create company website with routing

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav
+      style={{
+        padding: "15px",
+        backgroundColor: "#222",
+        display: "flex",
+        gap: "20px"
+      }}
+    >
+      <Link to="/" style={{ color: "#fff", textDecoration: "none" }}>
+        Home
+      </Link>
+      <Link to="/about" style={{ color: "#fff", textDecoration: "none" }}>
+        About
+      </Link>
+      <Link to="/services" style={{ color: "#fff", textDecoration: "none" }}>
+        Services
+      </Link>
+      <Link to="/contact" style={{ color: "#fff", textDecoration: "none" }}>
+        Contact
+      </Link>
+    </nav>
+  );
+}
+
+export default Navbar;
