9ce25bc1f4b4ad7fb803e08825994af50757f46e
maryblessing-tech <isolokwu.obiagelimaryblessing@gmail.com>
2026-01-19 20:52:28 +0100

    Move recipeStore to src directory and update imports

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,4 @@
-import useRecipeStore from '../store/recipeStore';
+import useRecipeStore from '../recipeStore';  // Change from '../store/recipeStore'
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
